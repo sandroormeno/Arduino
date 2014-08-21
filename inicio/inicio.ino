4194f0d86e9f3eadeb2da244f346cfdc965bbd2b
sandroormeno <sandro_alex@hotmail.com>
2014-08-21 11:05:02 -0500

    Primer ejemplo de Arduino
    Estructura básica de código de Arduino

---
+++ inicio/inicio.ino
@@ -0,0 +1,12 @@
+/*
+Ejemplo de estructura 
+básica de código de Arduino
+*/
+void setup() {                
+ //Esta parte del  código se ejecuta una sola vez.
+}
+
+
+void loop() {
+ //Esta parte del  código se repite indefinidamente.
+}
